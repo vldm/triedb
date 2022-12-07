31d35e8e4d856dc8f90e08e2dbe36b3dd81ff95a
Vladimir Motylenko <me@vldm.cc>
2022-12-07 16:28:44 +0200

    Optimise nibble slice.

--- src/merkle/nibble.rs
+++ src/merkle/nibble.rs
@@ -1,6 +1,10 @@
 //! Merkle nibble types.
 
-use std::{cmp::min, hash::Hash};
+use std::{
+    cmp::min,
+    hash::Hash,
+    ops::{Bound, RangeBounds},
+};
 
 use rlp::{Rlp, RlpStream};
 
@@ -250,6 +254,159 @@ pub fn common_all<'a, T: Iterator<Item = NibbleSlice<'a>>>(mut iter: T) -> Nibbl
     common_cur
 }
 
+// struct
+trait NibbleIndex {
+    type Slice<'b>
+    where
+        Self: 'b;
+    fn get_nibble(&self, index: usize) -> Option<Nibble>;
+
+    fn get_slice<R>(&self, range: R) -> Self::Slice<'_>
+    where
+        R: RangeBounds<usize>;
+    fn is_even(&self) -> bool;
+}
+
+impl NibbleIndex for NibbleVec {
+    type Slice<'b> = NibbleSlice<'b>;
+    fn get_nibble(&self, index: usize) -> Option<Nibble> {
+        self.get(index).copied()
+    }
+    fn is_even(&self) -> bool {
+        self.len() % 2 == 0
+    }
+
+    // duplicate because we cant propagate this method to NibbleSlice::get_slice implementation.
+    // because it's &self require additional temp reference
+    fn get_slice<R>(&self, range: R) -> Self::Slice<'_>
+    where
+        R: RangeBounds<usize>,
+    {
+        match (range.start_bound(), range.end_bound()) {
+            (Bound::Unbounded, Bound::Unbounded) => &self[..],
+            (Bound::Unbounded, Bound::Included(&x)) => &self[..=x],
+            (Bound::Unbounded, Bound::Excluded(&x)) => &self[..x],
+            (Bound::Included(&x), Bound::Unbounded) => &self[x..],
+            (Bound::Included(&x), Bound::Included(&y)) => &self[x..=y],
+            (Bound::Included(&x), Bound::Excluded(&y)) => &self[x..y],
+            (Bound::Excluded(_), _) => unreachable!(),
+        }
+    }
+}
+
+impl<'a> NibbleIndex for NibbleSlice<'a> {
+    type Slice<'b> = NibbleSlice<'b>
+    where Self: 'b
+    ;
+    fn get_nibble(&self, index: usize) -> Option<Nibble> {
+        self.get(index).copied()
+    }
+    fn is_even(&self) -> bool {
+        self.len() % 2 == 0
+    }
+    fn get_slice<R>(&self, range: R) -> Self::Slice<'_>
+    where
+        R: RangeBounds<usize>,
+    {
+        match (range.start_bound(), range.end_bound()) {
+            (Bound::Unbounded, Bound::Unbounded) => &self[..],
+            (Bound::Unbounded, Bound::Included(&x)) => &self[..=x],
+            (Bound::Unbounded, Bound::Excluded(&x)) => &self[..x],
+            (Bound::Included(&x), Bound::Unbounded) => &self[x..],
+            (Bound::Included(&x), Bound::Included(&y)) => &self[x..=y],
+            (Bound::Included(&x), Bound::Excluded(&y)) => &self[x..y],
+            (Bound::Excluded(_), _) => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct MyNibbleSlice<'a> {
+    bytes: &'a [u8],
+
+    skip_first: bool,
+    skip_last: bool,
+}
+
+impl<'a> MyNibbleSlice<'a> {
+    // Get slice from key bytes
+    // Key bytes always has first nibble.
+    // But we can allow skip last nibble.
+    fn from_raw_key(bytes: &'a [u8], even: bool) -> Self {
+        Self {
+            bytes,
+            skip_first: false,
+            skip_last: !even,
+        }
+    }
+    // Get slice from rlp representation
+    // if even, it always contain first garbage byte which can be skipped on creation.
+    // If odd, it contain only half byte of garbage, it should be skipped through skip_first flag
+    // The latest byte is always two nibbles.
+    fn from_raw_rlp(bytes: &'a [u8], even: bool) -> Self {
+        if even {
+            Self {
+                bytes: &bytes[1..],
+                skip_first: false,
+                skip_last: false,
+            }
+        } else {
+            Self {
+                bytes,
+                skip_first: true,
+                skip_last: false,
+            }
+        }
+    }
+
+    fn pos(&self, mut index: usize) -> (usize, bool) {
+        if self.skip_first {
+            index = index.saturating_add(1);
+        };
+        let index_in_bytes = index / 2;
+        let even = index % 2 != 0;
+        (index_in_bytes, even)
+    }
+
+    fn len(&self) -> usize {
+        let mut len = self.bytes.len() * 2;
+        if self.skip_first {
+            len -= 1;
+        }
+        if self.skip_last {
+            len -= 1;
+        }
+        len
+    }
+}
+impl<'a> NibbleIndex for MyNibbleSlice<'a> {
+    type Slice<'b> = MyNibbleSlice<'b>
+    where Self: 'b
+    ;
+    fn is_even(&self) -> bool {
+        self.skip_first == self.skip_last
+    }
+    fn get_nibble(&self, index: usize) -> Option<Nibble> {
+        if index >= self.len() {
+            return None;
+        }
+        let (index_in_bytes, even) = dbg!(self.pos(index));
+        self.bytes.get(index_in_bytes).map(|v| {
+            if even {
+                Nibble::from(*v & 0x0f)
+            } else {
+                Nibble::from((*v & 0xf0) >> 4)
+            }
+        })
+    }
+    fn get_slice<R>(&self, range: R) -> Self::Slice<'_>
+    where
+        R: RangeBounds<usize>,
+    {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -259,5 +416,94 @@ mod tests {
         let key = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 244, 233, 188];
 
         assert_eq!(key, into_key(&from_key(&key)));
+    }
+    #[test]
+    fn simple_get() {
+        let mut nibbles = NibbleVec::new();
+
+        nibbles.push(Nibble::N10);
+        nibbles.push(Nibble::N11);
+        nibbles.push(Nibble::N12);
+
+        assert_eq!(nibbles.len(), 3);
+        assert!(!nibbles.is_even());
+        nibbles.push(Nibble::N13);
+        nibbles.push(Nibble::N14);
+        nibbles.push(Nibble::N15);
+        assert_eq!(nibbles.len(), 6);
+        assert!(nibbles.is_even());
+
+        assert_eq!(nibbles.get_nibble(0), Some(Nibble::N10));
+        assert_eq!(nibbles.get_nibble(1), Some(Nibble::N11));
+        assert_eq!(nibbles.get_nibble(2), Some(Nibble::N12));
+
+        assert_eq!(nibbles.get_nibble(3), Some(Nibble::N13));
+        assert_eq!(nibbles.get_nibble(4), Some(Nibble::N14));
+        assert_eq!(nibbles.get_nibble(5), Some(Nibble::N15));
+        assert_eq!(nibbles.get_nibble(5), Some(Nibble::N15));
+        assert_eq!(nibbles.get_nibble(6), None);
+    }
+
+    #[test]
+    fn simple_get_my_nibble() {
+        let key = vec![0x12, 0xde, 0xad];
+        let my_nibble = MyNibbleSlice::from_raw_key(&key, true);
+        assert!(my_nibble.is_even());
+        assert!(!my_nibble.skip_first);
+        assert!(!my_nibble.skip_last);
+        assert_eq!(my_nibble.get_nibble(0), Some(Nibble::N1));
+        assert_eq!(my_nibble.get_nibble(1), Some(Nibble::N2));
+
+        assert_eq!(my_nibble.get_nibble(2), Some(Nibble::N13));
+        assert_eq!(my_nibble.get_nibble(3), Some(Nibble::N14));
+
+        assert_eq!(my_nibble.get_nibble(4), Some(Nibble::N10));
+        assert_eq!(my_nibble.get_nibble(5), Some(Nibble::N13));
+        assert_eq!(my_nibble.get_nibble(6), None);
+    }
+
+    #[test]
+    fn rlp_slice_encoded() {
+        let mut nibbles = NibbleVec::new();
+
+        nibbles.push(Nibble::N10);
+        nibbles.push(Nibble::N11);
+        nibbles.push(Nibble::N12);
+
+        assert_eq!(nibbles.len(), 3);
+        assert!(!nibbles.is_even());
+
+        let mut rlp = RlpStream::new();
+        encode(&nibbles, NibbleType::Leaf, &mut rlp);
+        let bytes = rlp.as_raw();
+        // first byte is rlp len - skip it
+        let my_nibble = MyNibbleSlice::from_raw_rlp(&bytes[1..], false);
+        dbg!(&my_nibble);
+        assert_eq!(my_nibble.get_nibble(0), Some(Nibble::N10));
+        assert_eq!(my_nibble.get_nibble(1), Some(Nibble::N11));
+        assert_eq!(my_nibble.get_nibble(2), Some(Nibble::N12));
+
+        assert_eq!(my_nibble.get_nibble(3), None);
+
+        nibbles.push(Nibble::N13);
+        nibbles.push(Nibble::N14);
+        nibbles.push(Nibble::N15);
+        assert_eq!(nibbles.len(), 6);
+        assert!(nibbles.is_even());
+
+        let mut rlp = RlpStream::new();
+        encode(&nibbles, NibbleType::Leaf, &mut rlp);
+        let bytes = rlp.as_raw();
+        // first byte is rlp len - skip it
+        let my_nibble = MyNibbleSlice::from_raw_rlp(&bytes[1..], true);
+
+        assert_eq!(my_nibble.get_nibble(0), Some(Nibble::N10));
+        assert_eq!(my_nibble.get_nibble(1), Some(Nibble::N11));
+        assert_eq!(my_nibble.get_nibble(2), Some(Nibble::N12));
+        assert_eq!(my_nibble.get_nibble(3), Some(Nibble::N13));
+        assert_eq!(my_nibble.get_nibble(4), Some(Nibble::N14));
+        assert_eq!(my_nibble.get_nibble(5), Some(Nibble::N15));
+
+        assert_eq!(my_nibble.get_nibble(6), None);
     }
 }
